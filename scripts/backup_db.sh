7b03c9d2765b25a3a17edd9e4cc05aa3ca94b9b6
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2023-11-06 00:06:29 -0500

    new tasks setup for dev container

---
+++ scripts/backup_db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -a # automatically export all variables
+source .env
+set +a
+
+export PGPASSWORD='JoeMontana2024'
+# Now you can use the environment variables in your script
+pg_dump -U "$DB_USER" -h "$DB_HOST" -f "./db-backups/dev-container-db-$(date +\%Y\%m\%d\%H\%M).sql" "$DB_NAME"
